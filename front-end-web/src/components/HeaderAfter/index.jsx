be09b60bd2d9cda7181ec27c7a4f9309cfd3fc9e
서정빈 <jeongb9716@gmail.com>
2023-09-13 10:54:36 +0900

    <first>[#368-SJB] Page별 폴더 생성

---
+++ front-end-web/src/components/HeaderAfter/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { NavLink, useNavigate } from 'react-router-dom';
+
+
+function Header() {
+  return (
+    <div>
+        
+    </div>
+  );
+}
+
+export default Header;

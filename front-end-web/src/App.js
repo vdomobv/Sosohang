932205c54129affdf87a147cb5c98990b13075b5
서정빈 <jeongb9716@gmail.com>
2023-09-13 10:56:32 +0900

    <feat>[#368-SJB] 각 페이지 연결

--- front-end-web/src/App.js
+++ front-end-web/src/App.js
@@ -1,13 +1,16 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import './App.css';
 
+import NotFound from "./pages/NotFound/index.jsx"
 import Main from "./pages/Main/index.jsx"
+import Login from "./pages/Login/index.jsx"
+import SignUp from "./pages/SignUp/index.jsx"
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Main />,
-  }
+  { path: "*", element: <NotFound /> },
+  { path: "/", element: <Main /> },
+  { path: "/login", element: <Login /> },
+  { path: "/signup", element: <SignUp />}
 ])
 
 function App() {

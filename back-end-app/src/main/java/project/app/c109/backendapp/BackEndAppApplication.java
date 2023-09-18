cdebce27e4a5558e7b1e1d41c13f2d23e81f27e8
이민규 <leeminkyu1212@gmail.com>
2023-09-18 11:39:14 +0900

    <first>[#336-LMK] 개발 환경 표준화
    S90P22C109/back-end-app
    서버 app 개발을 진행하기 위해 스켈레톤 코드를 정의하였습니다.

---
+++ back-end-app/src/main/java/project/app/c109/backendapp/BackEndAppApplication.java
@@ -0,0 +1,13 @@
+package project.app.c109.backendapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndAppApplication.class, args);
+	}
+
+}
---
+++ back-end-app/src/test/java/project/app/c109/backendapp/BackEndAppApplicationTests.java
@@ -0,0 +1,13 @@
+package project.app.c109.backendapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

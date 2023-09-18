cbd30d5f4831b90ac06f3c0594b90ea435eef3fa
이민규 <leeminkyu1212@gmail.com>
2023-09-18 11:37:50 +0900

    <first>[#336-LMK] 개발 환경 표준화
    S90P22C109/back-end-web
    서버 web 개발을 진행하기 위해 스켈레톤 코드를 정의하였습니다.

---
+++ back-end-web/src/test/java/project/web/c109/backendweb/BackEndWebApplicationTests.java
@@ -0,0 +1,13 @@
+package project.web.c109.backendweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

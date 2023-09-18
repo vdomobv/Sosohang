70e5a6c9f488bb768ba11d4ae637b642482c618e
김진주 <zizu4256@naver.com>
2023-09-18 18:00:19 +0900

    <feat>[#385-KJJ] 선물포장하기 페이지-메시지 작성 버튼 및 subtitle 생성

--- front-end-customer/src/Screens/MakeCard/MakeCard.js
+++ front-end-customer/src/Screens/MakeCard/MakeCard.js
@@ -1,21 +1,86 @@
 import styles from "./styles";
-import { View, Text, } from "react-native";
-
+import { View, Text, TouchableOpacity, TextInput } from "react-native";
+import React, { useState } from "react";
 
 export default function MakeCard({ route, navigation }) {
 console.log(route.params.selectedProducts);
+
+const [selectedButton, setSelectedButton] = useState(null);
+
+const handleButtonClick = (button) => {
+  if (selectedButton === button) {
+    setSelectedButton(null);
+  } else {
+    setSelectedButton(button);
+  }
+};
+
   return (
     <>
       <View style={styles.container}>
         <Text style={styles.title}>선물포장하기</Text>
-        <Text style={styles.subtitle}>메시지카드 작성</Text>
+        <View style={styles.subcontainer} >
+        <Text style={styles.subtitle}>📝 메시지카드 작성</Text>
         
-        <Text style={styles.subtitle}>보내는 사람</Text>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity
+            style={[
+              styles.button,
+              selectedButton === "+" ? styles.selectedButton : null,
+            ]}
+            onPress={() => handleButtonClick("+")}
+          >
+            <Text style={styles.buttonText}>+</Text>
+          </TouchableOpacity>
 
-        <Text style={styles.subtitle}>받는 사람</Text>
-        
-        <Text style={styles.subtitle}>상품 내역</Text>
-        
+          <TouchableOpacity
+            style={[
+              styles.button,
+              selectedButton === "생일" ? styles.selectedButton : null,
+            ]}
+            onPress={() => handleButtonClick("생일")}
+          >
+            <Text style={styles.buttonText}>생일</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={[
+              styles.button,
+              selectedButton === "감사" ? styles.selectedButton : null,
+            ]}
+            onPress={() => handleButtonClick("감사")}
+          >
+            <Text style={styles.buttonText}>감사</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={[
+              styles.button,
+              selectedButton === "응원" ? styles.selectedButton : null,
+            ]}
+            onPress={() => handleButtonClick("응원")}
+          >
+            <Text style={styles.buttonText}>응원</Text>
+          </TouchableOpacity>
+        </View>
+        </View>
+        <View style={styles.subcontainer} >
+        <Text style={styles.subtitle}>😊 보내는 사람 👉</Text>
+
+        <TextInput
+        style={styles.input}
+        placeholder="상대방에게 표시되는 이름이에요."
+
+        // onChangeText={(text) => setPassword(text)}
+        // value={sendname}
+      />
+      </View>
+      <View style={styles.subcontainer} >
+        <Text style={styles.subtitle}>😍 받는 사람 🖐</Text>
+        </View>
+        <View style={styles.subcontainer} >
+        <Text style={styles.subtitle}>🎁 상품 내역</Text>
+        </View>
       </View>
     </>
   );
--- front-end-customer/src/Screens/MakeCard/styles.js
+++ front-end-customer/src/Screens/MakeCard/styles.js
@@ -7,6 +7,10 @@ const styles = StyleSheet.create({
     marginTop: 25,
   },
 
+  subcontainer: {
+    marginVertical: 12,
+  },
+
   title: {
     fontSize: 21,
     marginTop: 10,
@@ -19,6 +23,45 @@ const styles = StyleSheet.create({
     fontSize: 20,
     marginLeft: 20,
     fontWeight: 'bold',
+  },
+
+  buttonContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: 'space-around',
+    marginHorizontal: 30,
+  },
+
+  button: {
+    backgroundColor: '#E9EEE8',
+    width: 70,
+    height: 50,
+    padding: 10,
+    borderRadius: 15,
+    margin: 10,
+    elevation: 10, // Add elevation for Android shadow
+  },
+
+  selectedButton: {
+    backgroundColor: '#E6FEDA',
+  },
+
+  buttonText: {
+    textAlign: 'center',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+
+  input: {
+    borderWidth: 1,
+    borderColor: 'gray',
+    borderRadius: 5,
+    padding: 8,
+    paddingLeft: 10,
+    margin: 10,
+    marginHorizontal: 20,
+
+    // width: '90%',
   },
 
 });

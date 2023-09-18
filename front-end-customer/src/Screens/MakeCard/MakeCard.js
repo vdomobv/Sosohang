a8b8c6e7b8ead0642cb3e5c2b05d61a8691a0b22
김진주 <zizu4256@naver.com>
2023-09-18 12:07:00 +0900

    <feat>[#385-KJJ] MakeCard 선물포장하기 페이지 생성

---
+++ front-end-customer/src/Screens/MakeCard/MakeCard.js
@@ -0,0 +1,23 @@
+import styles from "./styles";
+import { View, Text, } from "react-native";
+
+
+export default function MakeCard() {
+
+  return (
+    <>
+      <View style={styles.container}>
+        <Text style={styles.title}>선물포장하기</Text>
+        <Text style={styles.subtitle}>메시지카드 작성</Text>
+        
+        <Text style={styles.subtitle}>보내는 사람</Text>
+
+        <Text style={styles.subtitle}>받는 사람</Text>
+        
+        <Text style={styles.subtitle}>상품 내역</Text>
+        
+      </View>
+    </>
+  );
+}
+

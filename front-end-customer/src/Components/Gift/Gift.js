b75f3a1d4da9691bfd8b0e7d9a8f84b99ea597cf
vdomobv <vdomobv@naver.com>
2023-09-19 11:48:50 +0900

    <feat>[#346-LJE] Gift 컴포넌트 선물 수신, 발신 모두 쓸수 있도록 수정

--- front-end-customer/src/Components/Gift/Gift.js
+++ front-end-customer/src/Components/Gift/Gift.js
@@ -1,19 +1,33 @@
 import styles from "./styles";
 import { View, Text, Image } from "react-native";
 
+import CustomButton from "../../Components/CustomButton/CustomButton";
+
 export default function Gift({ navigation, data }) {
   return (
     <View style={styles.container}>
-      <Text style={styles.header}>from. {data.from} </Text>
+      <View style={styles.header}>
+        <Text style={styles.person}>
+          {data["from"] ? " from. " + data.from : " to. " + data.to}{" "}
+        </Text>
+        <Text style={styles.date}>{data.createdAt}</Text>
+      </View>
       <View style={styles.body}>
         <Image style={styles.image} source={data.image} />
         <View style={styles.contents}>
           <Text style={styles.shopName}>{data.shopname}</Text>
           <Text style={styles.name}>{data.name}</Text>
-          <Text style={styles.price}>남은 금액 : {data.price} 원</Text>
+          <Text style={styles.price}>
+            {data["from"] ? "남은 금액 : " + data.price : data.price} 원
+          </Text>
         </View>
       </View>
-      <Text style={styles.date}>{data.createdAt}</Text>
+      {data.to ? (
+        <View style={styles.buttons}>
+          <CustomButton content={"취소하기"} bgcolor="#FFBF46" />
+          <CustomButton content={"재주문"} />
+        </View>
+      ) : undefined}
     </View>
   );
 }

c3c6899178fffd2ec8d340a96535fa4e90aebe04
vdomobv <vdomobv@naver.com>
2023-09-19 10:27:19 +0900

    <feat>[#351-LJE] 캐러셀 아이템, 캐러셀 생성

--- front-end-customer/src/Components/Carousel/styles.js
+++ front-end-customer/src/Components/Carousel/styles.js
@@ -3,20 +3,8 @@ import { StyleSheet } from "react-native";
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    width : 100,
-    marginRight : 10,
+    marginVertical: 5,
   },
-
-  imageStyle : {
-    width : 100,
-    height : 100,
-    borderRadius : 10,
-  },
-
-  name : {
-    fontWeight : 'bold',
-    fontSize : 15,
-  }
 });
 
 export default styles

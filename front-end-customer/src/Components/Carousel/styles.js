e2741a379f51ddad22190ec9b9f3a03d079f5395
vdomobv <vdomobv@naver.com>
2023-09-19 11:50:02 +0900

    <fix>[#346-LJE] 캐러샐 flex 삭제

--- front-end-customer/src/Components/Carousel/styles.js
+++ front-end-customer/src/Components/Carousel/styles.js
@@ -2,9 +2,8 @@ import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     marginVertical: 5,
   },
 });
 
-export default styles
+export default styles;

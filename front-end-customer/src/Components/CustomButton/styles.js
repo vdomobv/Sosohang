dd5b11094f862d11e094d0f4b381f12352613ad7
vdomobv <vdomobv@naver.com>
2023-09-19 11:48:05 +0900

    <feat>[#346-LJE] CustomButton 생성

---
+++ front-end-customer/src/Components/CustomButton/styles.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    borderRadius: 5,
+    marginHorizontal: 2,
+    marginTop: 5,
+    paddingVertical: 10,
+  },
+
+  content: {
+    textAlign: "center",
+    fontSize: 16,
+  },
+});
+
+export default styles;

8121eb303d6808a693626b92763ba134d74b0f1a
vdomobv <vdomobv@naver.com>
2023-09-20 16:00:49 +0900

    <feat>[#367-LJE] 지도 기본 설정

--- front-end-customer/src/Screens/Map/Map.js
+++ front-end-customer/src/Screens/Map/Map.js
@@ -1,15 +1,38 @@
 import styles from "./styles";
-import { View, Text } from "react-native";
+import { View, Text, Alert } from "react-native";
+
+import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
+
+import * as Location from "expo-location";
+import { useEffect } from "react";
 
 export default function Map() {
-    return (
-      <View style={styles.container}>
-        <Text>지도</Text>
-        
-      <Tabs navigation={navigation}/>
-      </View>
+  const getLocation = async () => {
+    try {
+      const response = await Location.requestForegroundPermissionsAsync();
+      console.log(response);
+      const location = await Location.getCurrentPositionAsync();
+      console.log(location);
+    } catch (err) {
+      Alert.alert("위치 접근을 허용해주세요.");
+    }
+  };
 
-    );
-  }
-  
-  
+  useEffect(() => {
+    getLocation();
+  });
+
+  return (
+    <View>
+      <MapView
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+        provider={PROVIDER_GOOGLE}
+      />
+    </View>
+  );
+}

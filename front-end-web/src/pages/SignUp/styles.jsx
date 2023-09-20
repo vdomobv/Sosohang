a6dc2f3adf84de0814ab6119dd52b474e9ed6147
서정빈 <jeongb9716@gmail.com>
2023-09-20 17:57:18 +0900

    <feat>[#368-SJB] 회원가입 페이지 생성

--- front-end-web/src/pages/SignUp/styles.jsx
+++ front-end-web/src/pages/SignUp/styles.jsx
@@ -6,7 +6,8 @@ const Wrapper = styled.div`
 form {
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  justify-content: space-around;
+  align-items: center;
 }
 
 /* 필수정보 입력란 CSS */
@@ -18,12 +19,50 @@ form {
   height: 500px;
 }
 
+/* 상점정보, 사장님정보 입력란 CSS */
 .essentialInputBox {
+  background-color: lightgray;
+  padding: 10px;
+  border-radius: 5%;
+  width: 450px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: stretch;
+}
+
+/* 상점부가정보 CSS */
+.optionalInputBox {
+  width: 1000px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  /* align-items: center; */
+}
+
+/* 경고메세지 CSS */
+.waringMessage {
+  height: 24px;
+  margin: none;
+  display: inline-block;
+  color: red;
+}
+
+*:focus {
+  outline: none;
+  box-shadow: none;
+}
+
+.modalBox {
+  top: 15%;
+}
+
+.comboBox{
   display: flex;
   flex-direction: column;
   justify-content: space-around;
+  height: 150px;
 }
-  
 `;
 
 export default Wrapper;

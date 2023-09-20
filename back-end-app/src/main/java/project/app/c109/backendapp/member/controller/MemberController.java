9e4d94a218ca04c798b00336b04cc279d312213d
위효선 <wizardisme@naver.com>
2023-09-20 20:43:34 +0900

    <feat>[#403-WHS]회원가입 수정

--- back-end-app/src/main/java/project/app/c109/backendapp/member/controller/MemberController.java
+++ back-end-app/src/main/java/project/app/c109/backendapp/member/controller/MemberController.java
@@ -6,7 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
-import project.app.c109.backendapp.member.domain.dto.MemberFormDto;
+import project.app.c109.backendapp.member.domain.dto.SignUpFormDto;
 import project.app.c109.backendapp.member.domain.entity.Member;
 import project.app.c109.backendapp.member.service.MemberService;
 
@@ -14,7 +14,7 @@ import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/members")
-@Tag(name = "Template", description = "템플릿 API Document")
+@Tag(name = "MEMBER", description = "MEMBER API DOC")
 public class MemberController {
 
     private final MemberService memberService;
@@ -26,13 +26,13 @@ public class MemberController {
     /**
      * 회원 가입 요청 처리
      *
-     * @param memberFormDto 회원 가입 폼 데이터
+     * @param signUpFormDto 회원 가입 폼 데이터
      * @param bindingResult 입력 데이터 유효성 검사 결과
      * @return 회원 가입 결과 및 회원 정보
      */
     @PostMapping("/signup")
     @CrossOrigin(origins = "*", allowedHeaders = "*")
-    public ResponseEntity<?> signUp(@Valid @RequestBody MemberFormDto memberFormDto, BindingResult bindingResult) {
+    public ResponseEntity<?> signUp(@Valid @RequestBody SignUpFormDto signUpFormDto, BindingResult bindingResult) {
         // 입력 데이터 유효성 검사
         if (bindingResult.hasErrors()) {
             // BindingResult에서 오류 정보를 가져와서 응답으로 전달
@@ -44,7 +44,7 @@ public class MemberController {
         }
         // Member 엔터티 생성 및 저장
         try {
-            Member member = memberService.createMember(memberFormDto); // createMember 메서드 호출
+            Member member = memberService.createMember(signUpFormDto); // createMember 메서드 호출
             System.out.println("등록된 Member 정보: " + member);
             return ResponseEntity.status(HttpStatus.CREATED).body(member);
         } catch (IllegalStateException e) {

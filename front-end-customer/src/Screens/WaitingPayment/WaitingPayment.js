21bf0704b440ca8cdb55e6a7c0c0e5701114c9ce
eazsilver <vdomobv@naver.com>
2023-09-24 15:58:30 +0900

    <fix>[#367-LJE] react-native-webview 버전 수정 iamport-react-natvie 설치

--- front-end-customer/src/Screens/WaitingPayment/WaitingPayment.js
+++ front-end-customer/src/Screens/WaitingPayment/WaitingPayment.js
@@ -2,15 +2,20 @@ import styles from "./styles";
 import { Image, View, Text } from "react-native";
 
 import CustomButton from "../../Components/CustomButton/CustomButton"
+import { useEffect } from "react";
 
 
 export default function ScrollBox({ content }) {
+    useEffect(() => {
+        
+    }, [])
+
     return <View style={styles.container}>
         <View style={styles.image}>
             <Image source={require('assets/images/giftbox.gif')} />
         </View>
         <View style={styles.button}>
-            <CustomButton customStyles={{justifyContent : 'center'}} content={<Text style={styles.text}>결제 완료</Text>} />
+            <CustomButton customStyles={{ justifyContent: 'center' }} content={<Text style={styles.text}>결제 완료</Text>} />
         </View>
     </View>;
 }

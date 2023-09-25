3b2167cd30c18606c276156ab22478d4dbeb2a3f
wisdomx2 <wisdomx2@hanmail.net>
2023-09-25 10:45:19 +0900

    <refactor>[#395-YJH] 상품관련 코드 리팩토링 back-end-app/src/main/java/project/app/c109/backendapp/product web에 있던 product 디렉토리 코드 복사

--- back-end-app/src/main/java/project/app/c109/backendapp/product/domain/dto/request/ProductRequestDTO.java
+++ back-end-app/src/main/java/project/app/c109/backendapp/product/domain/dto/request/ProductRequestDTO.java
@@ -1,62 +1,17 @@
-package project.app.c109.backendapp.product.domain.entity;
+package project.app.c109.backendapp.product.domain.dto.request;
 
-import javax.persistence.*;
+public class ProductRequestDTO {
 
-// JPA Entity 선언
-@Entity
-// 데이터베이스에서의 테이블 이름
-@Table(name = "product")
-public class Product {
-
-    // Primary Key 선언
-    @Id
-    // Primary Key 생성 전략 (여기서는 auto-increment)
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    // 데이터베이스에서의 컬럼 이름
-    @Column(name = "product_seq")
-    private Long id;
-
-    // 상품명, null을 허용하지 않고 길이는 최대 50
-    @Column(name = "product_name", nullable = false, length = 50)
     private String productName;
-
-    // 정상가, null을 허용하지 않음
-    @Column(name = "product_price", nullable = false)
     private Integer productPrice;
-
-    // 할인율
-    @Column(name = "product_dcrate")
     private Integer productDcrate;
-
-    // 상품설명, null을 허용하지 않고 길이는 최대 2000
-    @Column(name = "product_info", nullable = false, length = 2000)
     private String productInfo;
-
-    // 사용기간, null을 허용하지 않고 길이는 최대 500 (50에서 늘렸음)
-    @Column(name = "product_exp", nullable = false, length = 500)
     private String productExp;
-
-    // 상품 이미지 URL, null을 허용하지 않고 길이는 최대 2000
-    @Column(name = "product_image", nullable = false, length = 2000)
     private String productImage;
-
-    // 수량
-    @Column(name = "product_count")
     private Integer productCount;
-
-    // 판매개수
-    @Column(name = "sales_amount")
     private Integer salesAmount;
 
     // 기본 생성자, getter, setter, etc...
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public String getProductName() {
         return productName;
     }
---
+++ back-end-app/src/main/java/project/app/c109/backendapp/product/repository/ProductRepository.java
@@ -0,0 +1,12 @@
+package project.app.c109.backendapp.product.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import project.app.c109.backendapp.product.domain.entity.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
+
+// JPA (Java Persistence API)를 사용하여 작성된 인터페이스
+// 별도의 메소드 구현이 없는 이유 :  Spring Data JPA가 제공하는 JpaRepository 인터페이스를 확장(extend)하고 있음
+// 기본 CRUD 연산을 위한 메소드들(save, findById, findAll, deleteById 등)이 이미 구현되어 있어,
+//ProductRepository 인터페이스는 이 기본 메소드들을 상속받아, 별도의 구현 없이도 사용할 수 있습니다.

a5fd0d3dd05dce6b50ec6b9bfcd915b5c5a43f63
vdomobv <vdomobv@naver.com>
2023-09-26 12:20:31 +0900

    <feat>{#449-LJE] 검색창 비활성화 구현

--- front-end-customer/src/Components/CustomSearchBar/CustomSearchBar.js
+++ front-end-customer/src/Components/CustomSearchBar/CustomSearchBar.js
@@ -1,9 +1,19 @@
 import styles from "./styles";
 import { SearchBar } from "@rneui/themed";
 
-export default function CustomSearchBar({ placeholderText }) {
+export default function CustomSearchBar({
+  placeholderText,
+  updateSearch,
+  updateSearchState,
+  cancelSearchState,
+  search,
+}) {
   return (
     <SearchBar
+      value={search}
+      onChangeText={updateSearch}
+      onFocus={updateSearchState}
+      onBlur={cancelSearchState}
       containerStyle={styles.containerStyle}
       inputContainerStyle={styles.inputContainerStyle}
       style={[styles.searchbar]}

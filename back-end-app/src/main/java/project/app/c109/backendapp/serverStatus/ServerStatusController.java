b9bb60bc7573d862889553260b335e1bd970d886
이민규 <leeminkyu1212@gmail.com>
2023-09-26 11:50:41 +0900

    [427-LMK] SERVER CHECK API 추가 >> >> back-end-app/src/main/java/project/app/c109/backendapp/serverStatus >> 자동 배포 성공 확인을 위해 api를 추가했습니다 .

--- back-end-app/src/main/java/project/app/c109/backendapp/serverStatus/ServerStatusController.java
+++ back-end-app/src/main/java/project/app/c109/backendapp/serverStatus/ServerStatusController.java
@@ -4,12 +4,25 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @RequestMapping("/api/v1/test")
+@Tag(name = "TEST", description = "SERVER STATUS CHECK")
 public class ServerStatusController {
 
 	@GetMapping
+	@Operation(summary = "Check server status", description = "Returns a message indicating server deployment status.")
+	@ApiResponses(value = {
+		@ApiResponse(responseCode = "200", description = "Server 배포 성공",
+			content = @Content(schema = @Schema(implementation = String.class))),
+		@ApiResponse(responseCode = "500", description = "Internal Server Error")
+	})
 	public ResponseEntity<String> checkServerStatus() {
 		return ResponseEntity.ok("Server 배포 성공");
 	}

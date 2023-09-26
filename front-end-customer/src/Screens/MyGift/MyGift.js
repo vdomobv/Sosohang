826d0cc6dfd45a8fda99647add85017751f90aeb
eazsilver <vdomobv@naver.com>
2023-09-26 22:32:55 +0900

    <feat>[#474-LJE] 받은 선물 사용가능, 사용완료 나누어 표시 구현

--- front-end-customer/src/Screens/MyGift/MyGift.js
+++ front-end-customer/src/Screens/MyGift/MyGift.js
@@ -1,5 +1,5 @@
 import styles from "./styles";
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 
 import Tabs from "../../Components/Tabs/Tabs";
 import Title from "../../Components/Title/Title";
@@ -7,10 +7,18 @@ import ScrollBox from "../../Components/ScrollBox/ScrollBox";
 import Gift from "../../Components/Gift/Gift";
 
 import MyGiftDummy from "../../Dummys/MyGift/MyGiftDummy";
+import SubTitle from "../../Components/SubTitle/SubTitle";
+import { useState } from "react";
 const dummy = MyGiftDummy;
 
 export default function MyGift({ navigation }) {
-  const gifts = dummy.map((d, index) => {
+  const [activatedTab, setActivatedTab] = useState(true)
+
+  const usableGifts = dummy.filter((d) => d.price > 0 === true).map((d, index) => {
+    return <Gift data={d} key={index} />;
+  });
+
+  const unusableGifts = dummy.filter((d) => d.price == 0 === true).map((d, index) => {
     return <Gift data={d} key={index} />;
   });
 
@@ -18,7 +26,27 @@ export default function MyGift({ navigation }) {
     <>
       <View style={styles.container}>
         <Title title={"받은 선물함"}></Title>
-        <ScrollBox content={gifts}/>
+        <View style={styles.giftTabs}>
+          <TouchableOpacity onPress={() => {
+            if (!activatedTab) {
+              setActivatedTab(!activatedTab)
+            }
+          }} style={[activatedTab ? styles.tab : styles.deactivated]}>
+            <SubTitle subTitle={'사용가능'} />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => {
+            if (activatedTab) {
+              setActivatedTab(!activatedTab)
+            }
+          }} style={[!activatedTab ? styles.tab : styles.deactivated]}>
+            <SubTitle subTitle={'사용완료'} />
+          </TouchableOpacity>
+        </View>
+        <View style={styles.tabPage}>
+          <ScrollBox content={
+            activatedTab ? usableGifts : unusableGifts
+          } />
+        </View>
       </View>
       <Tabs navigation={navigation} />
     </>

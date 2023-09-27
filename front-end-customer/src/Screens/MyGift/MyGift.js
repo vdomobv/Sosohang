92b636d8d6c1aff90c4c9510015fcbd2bcc44b5c
김진주 <zizu4256@naver.com>
2023-09-27 10:34:02 +0900

    <fix>[#497-KJJ] MyGift 페이지에서 상세페이지로 이동 코드 수정

--- front-end-customer/src/Screens/MyGift/MyGift.js
+++ front-end-customer/src/Screens/MyGift/MyGift.js
@@ -1,38 +1,29 @@
 import styles from "./styles";
+import React, { useState } from "react";
 import { View, TouchableOpacity } from "react-native";
 
-import Tabs from "../../Components/Tabs/Tabs";
 import Title from "../../Components/Title/Title";
+import SubTitle from "../../Components/SubTitle/SubTitle";
 import ScrollBox from "../../Components/ScrollBox/ScrollBox";
 import Gift from "../../Components/Gift/Gift";
+import Tabs from "../../Components/Tabs/Tabs";
 
 import MyGiftDummy from "../../Dummys/MyGift/MyGiftDummy";
-
-import SubTitle from "../../Components/SubTitle/SubTitle";
-import { useState } from "react";
 const dummy = MyGiftDummy;
 
 export default function MyGift({ navigation }) {
   const [activatedTab, setActivatedTab] = useState(true)
 
+  const handleGiftClick = (giftData) => {
+    navigation.navigate("MyGiftDetail", { giftData }); 
+  };
+  
   const usableGifts = dummy.filter((d) => d.price > 0 === true).map((d, index) => {
     return <Gift onPress={() => handleGiftClick(d)} data={d} key={index} />;
   });
   const unusableGifts = dummy.filter((d) => d.price == 0 === true).map((d, index) => {
     return <Gift onPress={() => handleGiftClick(d)} data={d} key={index} />;
   })
-
-  const handleGiftClick = (giftData) => {
-    navigation.navigate("MyGiftDetail", { giftData }); 
-  };
-
-  // const gifts = MyGiftDummy.map((d, index) => {
-  //   return (
-  //     <TouchableOpacity key={index} onPress={() => handleGiftClick(d)}>
-  //       <Gift data={d} />
-  //     </TouchableOpacity>
-  //   );
-  // });
 
 return (
   <>
--- front-end-customer/src/Screens/MyGiftDetail/MyGiftDetail.js
+++ front-end-customer/src/Screens/MyGiftDetail/MyGiftDetail.js
@@ -9,7 +9,7 @@ import Gift from "../../Components/Gift/Gift";
 import GiftCardBackground from "../../Components/GiftCardBackground/GiftCardBackground";
 
 export default function MyGiftDetail({ route, navigation }) {
-  const giftData = route.params.giftData;
+  const { giftData } = route.params;
 
   const content = {
     cardImage: "박스 1 카드이미지",
@@ -22,13 +22,12 @@ export default function MyGiftDetail({ route, navigation }) {
         <Title title={"받은 선물함"}></Title>
         <ScrollBox
           content={
-            <View>
+            <>
             <Gift data={giftData} />
             <View style={styles.subContainer}>
               <GiftCardBackground content={content}/>
             </View>
-            </View>
-
+            </>
           }
         />
       </View>

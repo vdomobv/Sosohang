9f4cd438f97ae733ff50409f2caae6ddbf2046f3
wisdomx2 <wisdomx2@hanmail.net>
2023-09-28 18:46:39 +0900

    <feat>[#432-YJH] 주문컨트롤러, DTO, 엔터티  기본생성 back-end-app/src/main/java/project/app/c109/backendapp/order 주문관련 기본 코드 생성

---
+++ back-end-app/src/main/java/project/app/c109/backendapp/order/domain/dto/response/OrderResponseDTO.java
@@ -0,0 +1,19 @@
+package project.app.c109.backendapp.order.domain.dto.response;
+
+import lombok.Data;
+import java.time.LocalDateTime;
+
+@Data
+public class OrderResponseDTO {
+    private Integer orderSeq;
+    private Integer categorySeq;
+    private Integer storeSeq;
+    private Integer memberSeq;
+    private Integer productSeq;
+    private LocalDateTime orderDate;
+    private Integer orderCount;
+    private Integer orderPrice;
+    private Integer orderStatus;
+    private Integer totalPrice;
+    private String paymentMethod;
+}
--- back-end-app/src/main/java/project/app/c109/backendapp/order/domain/entity/Order.java
+++ back-end-app/src/main/java/project/app/c109/backendapp/order/domain/entity/Order.java
@@ -1,4 +1,68 @@
 package project.app.c109.backendapp.order.domain.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+import project.app.c109.backendapp.product.domain.entity.Product;
+import project.app.c109.backendapp.store.domain.entity.Store;
+import project.app.c109.backendapp.member.domain.entity.Member;
+import project.app.c109.backendapp.category.domain.entity.Category;
+
+
+
+
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "order")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "order_seq")
+    private Integer orderSeq;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_seq")
+    private Category category;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "store_seq")
+    private Store store;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_seq")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_seq")
+    private Product product;
+
+    @Column(name = "order_date")
+    private LocalDateTime orderDate;
+
+    @Column(name = "order_count")
+    private Integer orderCount;
+
+    @Column(name = "order_price")
+    private Integer orderPrice;
+
+    @Column(name = "order_status")
+    private Integer orderStatus;  // 1. 정상, 2. 주문취소
+
+    @Column(name = "total_price")
+    private Integer totalPrice;
+
+    @Column(name = "payment_method")
+    private String paymentMethod;
+
+    // 연관관계 설정 등 추가 작업 진행 가능
 }

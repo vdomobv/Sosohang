02d92056ce6faa96e5a86fc940313efd5d5c9f41
서정빈 <jeongb9716@gmail.com>
2023-09-29 23:15:14 +0900

    <feat>[#428-SJB] 정산내역 Axios 연결

--- front-end-web/src/pages/StoreManage/index.jsx
+++ front-end-web/src/pages/StoreManage/index.jsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import ChartStoreSales from '../../components/ChartStoreSales';
+import TableStoreSettlement from "../../components/TableStoreSettlement";
 
 function  StoreManage() {
-  const salesData = {
-    months: ['1월', '2월', '3월', '4월', '5월', '6월'],
-    values: [100, 150, 200, 120, 180, 220],
-  };
 
   return (
     <div>
       <h1>상점관리페이지</h1>
-      <ChartStoreSales salesData={salesData} />
+      <TableStoreSettlement />
     </div>
   );
 }

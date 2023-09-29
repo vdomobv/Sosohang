d2c2e2f2b94af7d02a33c927023c6e4d2c565c53
서정빈 <jeongb9716@gmail.com>
2023-09-29 23:14:33 +0900

    <fix>[#428-SJB] 정산내역 보안이슈 수정

--- back-end-app/src/main/java/project/app/c109/backendapp/product/controller/ProductController.java
+++ back-end-app/src/main/java/project/app/c109/backendapp/product/controller/ProductController.java
@@ -2,8 +2,6 @@
 package project.app.c109.backendapp.product.controller;
 
 // 필요한 라이브러리와 패키지 import
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -25,26 +23,21 @@ import java.util.stream.Collectors;
 // 이 컨트롤러가 처리할 기본 URL 경로
 @RequestMapping("/api/v1/products")
 public class ProductController {
-    private static final Logger logger = LoggerFactory.getLogger(StoreController.class);
+  private final JwtUtils jwtUtils;
 
-	private final JwtUtils jwtUtils;
+	public ProductController(JwtUtils jwtUtils) {
+			this.jwtUtils = jwtUtils;
+	}
 
-    public ProductController(JwtUtils jwtUtils) {
-        this.jwtUtils = jwtUtils;
-    }
+	// ProductService를 자동 주입
+	@Autowired
+	private ProductService productService;
 
+	// 상점 내 상품 추가
+	@PostMapping
+	public ResponseEntity<ProductResponseDTO> addProduct(@CookieValue(name = "jwtToken") String cookieValue, @RequestBody ProductRequestDTO request) {
+		Integer storeSeq = -1;
 
-    // ProductService를 자동 주입
-    @Autowired
-    private ProductService productService;
-
-
-    // 상점 내 상품 추가
-    @PostMapping
-    public ResponseEntity<ProductResponseDTO> addProduct(@CookieValue(name = "jwtToken") String cookieValue, @RequestBody ProductRequestDTO request) {
-        Integer storeSeq = -1;
-		
-		logger.info(cookieValue);
 		if (cookieValue != null && cookieValue.startsWith("Bearer ")) {
 			cookieValue = cookieValue.substring(7); // "Bearer " 부분을 제외한 토큰 추출
 		}
@@ -58,17 +51,16 @@ public class ProductController {
 		} else {
 			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
 		}
-        
-        ProductResponseDTO response = productService.addProduct(storeSeq, request);
-        return ResponseEntity.ok(response);
-    }
+			
+		ProductResponseDTO response = productService.addProduct(storeSeq, request);
+		return ResponseEntity.ok(response);
+	}
 
-    // 상점 내 상품 수정
-    @PutMapping("/{productId}")
-    public ResponseEntity<ProductResponseDTO> updateProduct(@CookieValue(name = "jwtToken") String cookieValue, @PathVariable Integer productId, @RequestBody ProductRequestDTO request) {
-        Integer storeSeq = -1;
-		
-		logger.info(cookieValue);
+	// 상점 내 상품 수정
+	@PutMapping("/{productId}")
+	public ResponseEntity<ProductResponseDTO> updateProduct(@CookieValue(name = "jwtToken") String cookieValue, @PathVariable Integer productId, @RequestBody ProductRequestDTO request) {
+		Integer storeSeq = -1;
+
 		if (cookieValue != null && cookieValue.startsWith("Bearer ")) {
 			cookieValue = cookieValue.substring(7); // "Bearer " 부분을 제외한 토큰 추출
 		}
@@ -82,17 +74,16 @@ public class ProductController {
 		} else {
 			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
 		}
-        
-        ProductResponseDTO response = productService.updateProduct(storeSeq, productId, request);
-        return ResponseEntity.ok(response);
-    }
+			
+		ProductResponseDTO response = productService.updateProduct(storeSeq, productId, request);
+		return ResponseEntity.ok(response);
+	}
 
-    // 상점 내 상품 삭제
-    @DeleteMapping("/{productId}")
-    public ResponseEntity<Void> deleteProduct(@CookieValue(name = "jwtToken") String cookieValue, @PathVariable Integer productId) {
-        Integer storeSeq = -1;
-		
-		logger.info(cookieValue);
+	// 상점 내 상품 삭제
+	@DeleteMapping("/{productId}")
+	public ResponseEntity<Void> deleteProduct(@CookieValue(name = "jwtToken") String cookieValue, @PathVariable Integer productId) {
+		Integer storeSeq = -1; 
+
 		if (cookieValue != null && cookieValue.startsWith("Bearer ")) {
 			cookieValue = cookieValue.substring(7); // "Bearer " 부분을 제외한 토큰 추출
 		}
@@ -106,17 +97,16 @@ public class ProductController {
 		} else {
 			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
 		}
-        
-        productService.deleteProduct(storeSeq, productId);
-        return ResponseEntity.noContent().build();
-    }
+			
+		productService.deleteProduct(storeSeq, productId);
+		return ResponseEntity.noContent().build();
+	}
 
-    // 상점 내 모든 상품 조회
-    @GetMapping
-    public ResponseEntity<List<ProductResponseDTO>> getAllProductsByStoreForOwner(@CookieValue(name = "jwtToken") String cookieValue) {
-        Integer storeSeq = -1;
-		
-		logger.info(cookieValue);
+	// 상점 내 모든 상품 조회
+	@GetMapping
+	public ResponseEntity<List<ProductResponseDTO>> getAllProductsByStoreForOwner(@CookieValue(name = "jwtToken") String cookieValue) {
+		Integer storeSeq = -1;
+
 		if (cookieValue != null && cookieValue.startsWith("Bearer ")) {
 			cookieValue = cookieValue.substring(7); // "Bearer " 부분을 제외한 토큰 추출
 		}
@@ -129,17 +119,16 @@ public class ProductController {
 			} 
 		} else {
 			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-		}			
-
-        List<ProductResponseDTO> productDTOs = productService.getAllProductsByStore(storeSeq);
-        return ResponseEntity.ok(productDTOs);
-    }
-
-		@GetMapping("/{storeId}")
-    public ResponseEntity<List<ProductResponseDTO>> getAllProductsByStore(@PathVariable Integer storeId) {
-			List<ProductResponseDTO> productDTOs = productService.getAllProductsByStore(storeId);
-
-			return ResponseEntity.ok(productDTOs);
 		}
 
+		List<ProductResponseDTO> productDTOs = productService.getAllProductsByStore(storeSeq);
+		return ResponseEntity.ok(productDTOs);
+	}
+
+	@GetMapping("/{storeId}")
+	public ResponseEntity<List<ProductResponseDTO>> getAllProductsByStore(@PathVariable Integer storeId) {
+		List<ProductResponseDTO> productDTOs = productService.getAllProductsByStore(storeId);
+
+		return ResponseEntity.ok(productDTOs);
+	}
 }

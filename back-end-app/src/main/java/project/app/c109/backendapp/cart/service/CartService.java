2dc466852290172038cb613ad86ada989d408d4e
위효선 <wizardisme@naver.com>
2023-09-29 21:48:01 +0900

    <feat>[#283-WHS] 카트

--- back-end-app/src/main/java/project/app/c109/backendapp/cart/service/CartService.java
+++ back-end-app/src/main/java/project/app/c109/backendapp/cart/service/CartService.java
@@ -1,14 +1,21 @@
 package project.app.c109.backendapp.cart.service;
 
+
 import project.app.c109.backendapp.cart.domain.dto.request.CartRequestDTO;
 import project.app.c109.backendapp.cart.domain.dto.response.CartResponseDTO;
+import project.app.c109.backendapp.cart.domain.entity.Cart;
 
 import java.util.List;
 
+
 public interface CartService {
     CartResponseDTO addCart(CartRequestDTO request);
-    CartResponseDTO updateCart(Long cartId, CartRequestDTO request);
-    void deleteCart(Long cartId);
-    List<CartResponseDTO> getAllCarts();
-    // 기타 필요한 메서드를 추가하세요.
+
+    CartResponseDTO updateCartItemQuantity(CartRequestDTO request);
+
+    void deleteCartItem(Integer memberSeq, Integer productSeq);
+
+    List<Cart> getAllCartsByMemberSeq(Integer memberSeq);
+
+    boolean checkDuplicatedItem(Integer memberSeq, Integer productSeq);
 }

847ae563a3271a9459ffdddae0223dac9b05793a
서정빈 <jeongb9716@gmail.com>
2023-09-30 18:04:23 +0900

    <feat>[#428-SJB] 상점정산정보 수정 추가

--- front-end-web/src/pages/StoreManage/index.jsx
+++ front-end-web/src/pages/StoreManage/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ChartStoreSales from '../../components/ChartStoreSales';
 import TableStoreSettlement from "../../components/TableStoreSettlement";
+import Header from "../../components/Header"
 
 function  StoreManage() {
 
   return (
     <div>
+      <Header />
       <h1>상점관리페이지</h1>
       <TableStoreSettlement />
     </div>

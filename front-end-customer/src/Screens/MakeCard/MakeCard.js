4143fb76959c5c32a2569d3063b1f19bc8c040d9
eazsilver <vdomobv@naver.com>
2023-10-03 00:44:06 +0900

    <feat>[#462-LJE] 주문 API 연결

--- front-end-customer/src/Screens/MakeCard/MakeCard.js
+++ front-end-customer/src/Screens/MakeCard/MakeCard.js
@@ -83,7 +83,6 @@ export default function MakeCard({ route, navigation }) {
   const renderGroupedProducts = () => {
     return Object.keys(groupedByStore).map((storeSeq) => {
       const productsInShop = groupedByStore[storeSeq];
-      console.log('여기 : ', productsInShop[0])
       return (
         <View key={storeSeq}>
           <Text style={styles.shopName}>

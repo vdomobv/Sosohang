e999ab244cd8dec25278085809eecc41aa4c6112
서정빈 <jeongb9716@gmail.com>
2023-10-03 18:38:24 +0900

    <fix>[#428-SJB] 사장님 App Axios 수정

--- front-end-owner/src/Screens/LogIn/styles.js
+++ front-end-owner/src/Screens/LogIn/styles.js
@@ -9,6 +9,7 @@ const styles = StyleSheet.create({
 
   info: {
     textAlign: 'right',
+    fontSize: 16,
     marginTop: 40,
     marginRight: 40,
   },
@@ -17,22 +18,25 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderColor: 'gray',
     borderRadius: 5,
-    padding: 8,
+    padding: 12,
     margin: 5,
+    marginVertical: 8,
     width: '80%',
+    fontSize: 18,
   },
 
   button: {
     width: '80%',
     borderRadius: 5,
-    padding: 10,
-    marginTop: 10,
+    padding: 15,
+    marginTop: 15,
     backgroundColor: '#46C27D',
+    elevation: 5
   },
 
   buttonText: {
     color: '#ffffff', 
-    fontSize: 18, 
+    fontSize: 20, 
     fontWeight: 'bold', 
     textAlign: 'center', 
   }

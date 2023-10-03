982677e67eb80cc78e2930e722440198cfad7d53
김진주 <zizu4256@naver.com>
2023-10-04 00:02:10 +0900

    <feat>[#418-KJJ] src 상품 이미지 오류 수정

--- front-end-customer/src/Components/CarouselItem/CarouselItem.js
+++ front-end-customer/src/Components/CarouselItem/CarouselItem.js
@@ -5,7 +5,7 @@ export default function CarouselItem({ props, onPressFunction }) {
   // console.log('CarouselItem : ', props);
   return (
     <TouchableOpacity onPress={onPressFunction} style={styles.container}>
-      <Image style={styles.imageStyle} source={storeImage === 'string' ? require("assets/images/bread.png") : { uri: storeImage }} />
+      <Image style={styles.imageStyle} src={props.storeImage === 'string' ? "assets/images/bread.png" : props.storeImage } />
       <Text style={styles.name}>{props.storeName}</Text>
       <Text style={styles.address}>{props.storeLocation}</Text>
     </TouchableOpacity>

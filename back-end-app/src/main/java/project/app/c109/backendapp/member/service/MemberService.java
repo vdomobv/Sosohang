7396a65abd6e8cf77c33d3994fb2c7061f51071f
wisdomx2 <wisdomx2@hanmail.net>
2023-10-04 10:35:48 +0900

    <fix>[#375-YJH] 회원가입시 비회원 인증번호 문자로 발송 back-end-app/src/main/java/project/app/c109/backendapp/MemberService 회원가입할 때 가입한 사용자인지 아닌지 확인하여, 비회원인 경우 twilio로 문자 발송

--- back-end-app/src/main/java/project/app/c109/backendapp/member/service/MemberService.java
+++ back-end-app/src/main/java/project/app/c109/backendapp/member/service/MemberService.java
@@ -1,5 +1,11 @@
 package project.app.c109.backendapp.member.service;
 
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.exception.ApiException;
+import com.twilio.type.PhoneNumber;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -12,6 +18,7 @@ import project.app.c109.backendapp.member.domain.dto.response.LoginResponse;
 import project.app.c109.backendapp.member.domain.entity.Member;
 import project.app.c109.backendapp.member.repository.MemberRepository;
 
+import javax.annotation.PostConstruct;
 import javax.persistence.EntityExistsException;
 import javax.persistence.EntityNotFoundException;
 import java.util.concurrent.TimeUnit;
@@ -23,7 +30,19 @@ public class MemberService {
     private StringRedisTemplate stringRedisTemplate;
     private final PasswordEncoder passwordEncoder;
     private final JwtUtils jwtUtils;
+    @Autowired
+    private Environment env;
 
+    private String twilioAccountSid;
+    private String twilioAuthToken;
+    private String twilioPhoneNumber;
+
+    @PostConstruct
+    public void init() {
+        twilioAccountSid = env.getProperty("twilio.accountSid");
+        twilioAuthToken = env.getProperty("twilio.authToken");
+        twilioPhoneNumber = env.getProperty("twilio.phoneNumber");
+    }
 
     public MemberService(MemberRepository memberRepository, PasswordEncoder passwordEncoder, JwtUtils jwtUtils, StringRedisTemplate stringRedisTemplate) {
         this.memberRepository = memberRepository;
@@ -58,11 +77,45 @@ public class MemberService {
         return new LoginResponse(token, member);
     }
 
+//    public String handlePhoneVerification(String phoneNumber) {
+//        if(memberRepository.existsByMemberPhone(phoneNumber)) { return null; }
+//        String authCode = String.format("%06d", (int)(Math.random() * 1000000));
+//        stringRedisTemplate.opsForValue().set(phoneNumber, authCode, 10, TimeUnit.SECONDS);
+//        return authCode;
+//    }
+
     public String handlePhoneVerification(String phoneNumber) {
-        if(memberRepository.existsByMemberPhone(phoneNumber)) { return null; }
+        // 해당 핸드폰 번호가 이미 존재하는지 확인
+        if(memberRepository.existsByMemberPhone(phoneNumber)) {
+            return "이미 가입된 사용자입니다.";
+        }
+
+        // 6자리 인증 코드 생성
         String authCode = String.format("%06d", (int)(Math.random() * 1000000));
-        stringRedisTemplate.opsForValue().set(phoneNumber, authCode, 10, TimeUnit.SECONDS);
-        return authCode;
+
+        // Redis에 인증 코드 저장 (180초 동안)
+        stringRedisTemplate.opsForValue().set(phoneNumber, authCode, 180, TimeUnit.SECONDS);
+
+        // 인증 코드를 사용자의 휴대폰 번호로 전송
+        sendVerificationCodeViaSMS(phoneNumber, authCode);
+
+        return "인증번호 전송 완료!";
+    }
+
+    private void sendVerificationCodeViaSMS(String phoneNumber, String authCode) {
+        try {
+            Twilio.init(twilioAccountSid, twilioAuthToken);
+            String messageBody = "인증번호를 입력하세요: " + authCode;
+
+            Message message = Message.creator(
+                    new PhoneNumber(phoneNumber),
+                    new PhoneNumber(twilioPhoneNumber),
+                    messageBody
+            ).create();
+        } catch (ApiException e) {
+            System.err.println(e.getMessage());
+            throw new RuntimeException("Failed to send SMS", e);
+        }
     }
 
     public boolean verifyAuthCode(String phoneNumber, String inputAuthCode) {

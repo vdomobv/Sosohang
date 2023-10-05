e34f94525e702cad80f43905be14c4d2f3912d65
이민규 <leeminkyu1212@gmail.com>
2023-10-05 17:31:13 +0900

    <feat>[427-LMK] 앱 빌드 준비
    front-end-owner/app.json app.config.js package-lock.json
    owner 앱 빌드 준비중입니다.

--- front-end-owner/app.config.js
+++ front-end-owner/app.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   expo: {
     android: {
-      package: "com.sosohang.sosohang",
+      package: "com.sosohang_owner.sosohang_owner",
       config: {
         googleMaps: {
           apiKey: process.env.EXPO_GOOGLE_API_KEY,

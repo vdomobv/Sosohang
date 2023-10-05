a7b73e61b74c845bba2b76cf8b0325530fe8c8b2
vdomobv <vdomobv@naver.com>
2023-10-05 11:17:11 +0900

    <fix>[#462-LJE] 이미지 src => source로 변경

--- front-end-customer/src/Components/SquareImage/SquareImage.js
+++ front-end-customer/src/Components/SquareImage/SquareImage.js
@@ -2,7 +2,5 @@ import styles from "./styles";
 import { Image } from "react-native";
 
 export default function SquareImage({ imageSrc }) {
-  return (
-    <Image src={imageSrc} style={styles.image}/>
-  )
+  return <Image source={imageSrc} style={styles.image} />;
 }
